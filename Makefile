e789ab59144d8b1285c7527a0f4c0ac1d326d5ff
kline <kline@debian.(none)>
2011-12-08 15:34:22 -0600

    major update to building with debian packaging
    updated Makefile; added all,clean,install,deb targets
    
    updated changelog 1.3.0 version
    
    cut the dependencies to minimial in debian/control file, thanks
    to pdebuild and warnings in the new make system
    
    new addition of debian/source/format file (3.0 (quilt))
    
    udpate and simplification to setup.py; now all builds run through
    this script

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+#!/usr/bin/make -f
 # to build python-gridftp by hand in this dir, do
 #   make all
 #
@@ -9,48 +10,47 @@
 #
 # to build python-gridftp srpms for Scientific Linux, do
 #   make srpm
-#
-TARGET=gridftpwrapper.so
+
 RPMDIR=$(HOME)/rpmbuild
 VERSION=1.3.0
 SRCNAME=python-gridftp
-MY_INC=-I/usr/include/python2.6 
-OS=$(shell uname)
-MFH=$(wildcard makefile_header)
 
-ifeq ($(OS),Darwin)
-	MY_CC_OPTS=-bundle -undefined suppress -flat_namespace
-endif
-ifeq ($(OS),Linux)
-	MY_CC_OPTS=-shared
-endif
-
-all: $(TARGET) 
-
-makefile_header:
-	globus-makefile-header --flavor=gcc64pthr > $@
+all: 
+	@echo
+	@echo
+	@echo "Targets are:"
+	@echo "    all, clean, install, srpm, rpm and deb"
+	@echo
+	@echo "all: print this message"
+	@echo "clean: remove stuff"
+	@echo "install: python setup.py install --root=debian/python-gridftp --prefix=/usr"
+	@echo "srpm: clean and then build the source rpm"
+	@echo "rpm: clean and then build the rpm and source rpm"
+	@echo "deb: create an orig.tar.gz file and run debuild -uc -us"
+	@echo
+	@echo
 
 clean:
-	$(RM) -r *.so\
-		 *.o\
-		 *.pyc\
-		 $(RPMDIR)/*/$(SRCNAME)*\
-		 $(RPMDIR)/*/*/$(SRCNAME)*\
-		 build\
-		 dist\
-		 MANIFEST\
+	$(RM) -r\
+		$(RPMDIR)/*/$(SRCNAME)*\
+		$(RPMDIR)/*/*/$(SRCNAME)*\
+		../$(SRCNAME)_*.deb\
+		../$(SRCNAME)_*.dsc\
+		../$(SRCNAME)_*.changes\
+		../$(SRCNAME)_*.build\
+		../$(SRCNAME)_*.debian.*\
+		*.pyc\
+		build\
+		dist\
+		MANIFEST\
+		debian/python-gridftp\
+		debian/python-gridftp.*.log\
+		debian/python-gridftp.*.debhelper\
+		debian/python-gridftp.substvars\
+		debian/files
 
-sinclude makefile_header
-MY_CFLAGS   = $(GLOBUS_INCLUDES) $(GLOBUS_CFLAGS) $(MY_INC) -fPIC 
-MY_LDFLAGS  = $(GLOBUS_LDFLAGS)   $(GLOBUS_PKG_LDFLAGS) -lpython2.6 -fPIC
-MY_LIBS     = $(GLOBUS_LIBS) $(GLOBUS_PKG_LIBS)
-
-%.o: %.c
-	$(CC) $(MY_CFLAGS) -c $< -o $@
-
-%.so: %.o
-	$(CC) $(MY_CC_OPTS) -o $@ $< $(MY_LDFLAGS) $(MY_LIBS)
-
+install:
+	python setup.py install --root=debian/python-gridftp --prefix=/usr
 
 # Scientific Linux source rpm
 srpm: clean
@@ -66,3 +66,13 @@ srpm: clean
 rpm: srpm
 	rpmbuild -ba $(RPMDIR)/SPECS/$(SRCNAME).spec
 
+# debian commands to build deb files
+deb: clean
+	$(RM) ../$(SRCNAME)_$(VERSION).orig.tar.gz
+	tar -cf\
+		../$(SRCNAME)_$(VERSION).orig.tar\
+		--transform=s/\./$(SRCNAME)_$(VERSION)/\
+		--exclude-vcs\
+		.
+	gzip ../$(SRCNAME)_$(VERSION).orig.tar
+	debuild -rfakeroot -D -uc -us
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import platform
 from distutils.core import setup, Extension
 
 GLOBUS_LOCATION = "/usr"
-WORD_SIZE = None
 
 try:
     GLOBUS_LOCATION = os.environ["GLOBUS_LOCATION"]
@@ -12,13 +11,7 @@ except KeyError:
     print >> sys.stderr, "GLOBUS_LOCATION is not set; using default %s" %(GLOBUS_LOCATION)
 
 try:
-    processorString = platform.machine()
     platformString = platform.platform()
-
-    if processorString == 'x86_64' or '64bit' in platformString:
-        WORD_SIZE = 64
-    else:
-        WORD_SIZE = 32
 except Exception, e:
     print >> sys.stderr, "Unable to determine if using x86_64 processor"
     sys.exit(-1)
@@ -28,61 +21,12 @@ if platform.dist()[0] == 'debian':
 else:
         my_include_dirs=[ "/usr/include/globus", "/usr/lib64/globus/include" ]
     
-linkFlags32 = [
-"-L%s/lib" % GLOBUS_LOCATION,
-"-lglobus_ftp_client_gcc32dbgpthr",
-"-lglobus_ftp_control_gcc32dbgpthr",
-"-lglobus_usage_gcc32dbgpthr",
-"-lglobus_io_gcc32dbgpthr",
-"-lglobus_xio_gcc32dbgpthr",
-"-lgssapi_error_gcc32dbgpthr",
-"-lglobus_gss_assist_gcc32dbgpthr",
-"-lglobus_gssapi_gsi_gcc32dbgpthr",
-"-lglobus_gsi_proxy_core_gcc32dbgpthr",
-"-lglobus_gsi_credential_gcc32dbgpthr",
-"-lglobus_gsi_callback_gcc32dbgpthr",
-"-lglobus_oldgaa_gcc32dbgpthr",
-"-lglobus_gsi_sysconfig_gcc32dbgpthr",
-"-lglobus_gsi_cert_utils_gcc32dbgpthr",
-"-lglobus_openssl_gcc32dbgpthr",
-"-lglobus_openssl_error_gcc32dbgpthr",
-"-lglobus_callout_gcc32dbgpthr",
-"-lglobus_proxy_ssl_gcc32dbgpthr",
-"-lglobus_common_gcc32dbgpthr",
-"-lssl",
-"-lcrypto",
-"-lltdl_gcc32dbgpthr",
-]
-
-linkFlags64 = [
+linkFlags = [
 "-L%s/lib64" % GLOBUS_LOCATION,
 "-lglobus_ftp_client",
-"-lglobus_gass_transfer",
-"-lglobus_ftp_control",
-"-lglobus_usage",
 "-lglobus_io",
-"-lglobus_xio",
-"-lglobus_gssapi_gsi",
-"-lglobus_gsi_proxy_core",
-"-lglobus_gsi_credential",
-"-lglobus_gsi_callback",
-"-lglobus_oldgaa",
-"-lglobus_gsi_sysconfig",
-"-lglobus_gsi_cert_utils",
-"-lglobus_openssl",
-"-lglobus_openssl_error",
-"-lglobus_callout",
-"-lglobus_proxy_ssl",
 "-lglobus_common",
-"-lssl",
-"-lcrypto",
-"-lltdl",
 ]
-
-if WORD_SIZE == 64:
-    linkFlags = linkFlags64
-else:
-    linkFlags = linkFlags32
 
 e = Extension(
         "gridftpwrapper",
@@ -95,7 +39,7 @@ e = Extension(
 extModList = [e]
 
 setup(name="python-gridftp",
-      version="1.1.0",
+      version="1.3.0",
       description="Python GridFTP client bindings",
       author="Scott Koranda",
       author_email="ldr-lsc@gravity.phys.uwm.edu",
